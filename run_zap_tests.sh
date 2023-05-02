94ace0c5bc4620241feb1b84086858acc0a69850
Akhilesh Kumar <akhilesh.kumar@digital.hmrc.gov.uk>
2023-05-02 09:02:27 +0100

    Removed Bobby Voilation

--- run_zap_tests.sh
+++ run_zap_tests.sh
@@ -10,4 +10,3 @@ environment="local"
 
 sbt -Dzap.proxy=true -Denvironment="$environment" -Dbrowser="$browser" clean 'testOnly uk.gov.hmrc.test.ui.cucumber.runner.ZapRunner'
 
-sbt 'testOnly uk.gov.hmrc.test.ui.cucumber.runner.Pillar2ZapTestRunner'
