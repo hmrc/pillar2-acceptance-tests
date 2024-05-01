e8d1934fbd154040b0e12bb2d52e4b84815b8239
dhirenganvir <dhiren.ganvir@digital.hmrc.gov.uk>
2024-05-01 17:35:43 +0100

    Tiding up the code

--- src/test/scala/uk/gov/hmrc/test/ui/cucumber/stepdefs/CommonFunctions.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/cucumber/stepdefs/CommonFunctions.scala
@@ -150,7 +150,7 @@ trait CommonFunctions
       case "RFM Contact Details Final CYA Page"   => RFMFinalReviewCYAPage
       case "ASA Pillar2 Input Page"               => ASAPillar2InputPage
       case "RFM No record Match Error Page"       => RFMNoMatchErrorPage
-      case "RFM Saving Progress Page"             =>RFMSavingProgressPage
+      case "RFM Saving Progress Page"             => RFMSavingProgressPage
       case "ASA No record Match Error Page"       => ASANoMatchErrorPage
       case "ASA Confirmation Page"                => ASAConfirmationPage
 
