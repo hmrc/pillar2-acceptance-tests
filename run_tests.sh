e29ffc045897cf926275a3e54de70de0e001be69
annemonica5 <129970066+annemonica5@users.noreply.github.com>
2024-05-10 12:08:46 +0100

    [PIL-944][MA]Updated Runner File

--- run_tests.sh
+++ run_tests.sh
@@ -3,4 +3,4 @@
 ENV="local"
 BROWSER="chrome"
 
-sbt -Dlogback.configurationFile=logback.xml -Dbrowser=$BROWSER -Denvironment=$ENV -Daccessibility.test=true "testOnly uk.gov.hmrc.test.ui.cucumber.runner.Runner1" testReport
+sbt -Dlogback.configurationFile=logback.xml -Dbrowser=$BROWSER -Denvironment=$ENV -Daccessibility.test=true "testOnly uk.gov.hmrc.test.ui.cucumber.runner.Runner" testReport
