6df2f3ef48a07c20b0bcdf1476ac8f97af5f2baa
neelu-dwivedi <141658439+neelu-dwivedi@users.noreply.github.com>
2024-09-27 12:14:45 +0100

    [PIL-1125] [ND] updated test libraries and fixed accessibility report
    Signed-off-by: neelu-dwivedi <141658439+neelu-dwivedi@users.noreply.github.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,11 +3,11 @@ import sbt.*
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"          %% "ui-test-runner"        % "0.24.0",
+    "uk.gov.hmrc"          %% "ui-test-runner"        % "0.30.0",
     "org.slf4j"             % "slf4j-simple"          % "1.7.36",
-    "org.scalatest"        %% "scalatest"             % "3.2.17",
+    "org.scalatest"        %% "scalatest"             % "3.2.18",
     "org.scalatestplus"    %% "selenium-4-12"         % "3.2.17.0",
-    "com.vladsch.flexmark"  % "flexmark-all"          % "0.62.2",
+    "com.vladsch.flexmark"  % "flexmark-all"          % "0.64.8",
     "io.cucumber"          %% "cucumber-scala"        % "8.20.0",
     "io.cucumber"           % "cucumber-junit"        % "7.15.0",
     "junit"                 % "junit"                 % "4.13.2",
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -5,6 +5,6 @@ resolvers += Resolver.url("HMRC-open-artefacts-ivy", url("https://open.artefacts
 
 addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "3.22.0")
 
-addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.4.6")
+addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.5.2")
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-test-report" % "0.22.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-test-report" % "1.0.0")
