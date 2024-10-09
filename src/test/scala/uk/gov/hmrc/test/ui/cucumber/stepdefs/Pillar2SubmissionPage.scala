a603feec7aee86e8c9ebca624ac42e1cb0f570e9
srikanthhmrc <srikanth.madduri@digital.hmrc.gov.uk>
2024-10-09 12:04:18 +0100

    tidy up the code

--- src/test/scala/uk/gov/hmrc/test/ui/cucumber/stepdefs/Pillar2SubmissionPage.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/cucumber/stepdefs/Pillar2SubmissionPage.scala
@@ -21,7 +21,6 @@ import org.scalatest.concurrent.Eventually
 import org.scalatest.matchers.should.Matchers
 import uk.gov.hmrc.test.ui.cucumber.PageObject
 import uk.gov.hmrc.test.ui.driver.BrowserDriver
-import uk.gov.hmrc.test.ui.pages.{AccountsSummaryPage, P2SubAccountsSummaryPage}
 import uk.gov.hmrc.test.ui.pillar2SubmissionPages._
 
 trait Pillar2SubmissionPage
