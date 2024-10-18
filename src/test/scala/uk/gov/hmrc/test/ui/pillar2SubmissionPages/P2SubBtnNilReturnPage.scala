ecec5c245eb8918e774a17c69725b47221273896
Prashant-digital <180386844+Prashant-digital@users.noreply.github.com>
2024-10-18 11:00:19 +0100

    PIL-1098[PDS] work in progress for adding new nil return page

---
+++ src/test/scala/uk/gov/hmrc/test/ui/pillar2SubmissionPages/P2SubBtnNilReturnPage.scala
@@ -0,0 +1,8 @@
+package uk.gov.hmrc.test.ui.pillar2SubmissionPages
+
+import uk.gov.hmrc.test.ui.cucumber.PageObject
+
+object P2SubBtnNilReturnPage extends PageObject{
+  val url: String = s"$p2SubRootUrl"+"below-threshold-notification/nil-return"
+
+}
