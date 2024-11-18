8e8d29a4bb2ee7e4490ac8e86e5bf1c6091c5145
Richard Dowsett <180321016+ridow-hmrc@users.noreply.github.com>
2024-11-18 12:30:05 +0000

    Add API runner

---
+++ run_api_tests.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+ENV="local"
+BROWSER="chrome"
+
+sbt clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" "testOnly uk.gov.hmrc.test.ui.cucumber.runner.APIRunner" testReport
