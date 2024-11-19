9eb15a00d4912e490f3028e2679ba872b02d9058
Prashant-digital <180386844+Prashant-digital@users.noreply.github.com>
2024-11-19 16:52:16 +0000

    [PDS]- Work in progress

--- ui-tests/src/test/scala/uk/gov/hmrc/test/ui/cucumber/stepdefs/ApiLoginSteps.scala
+++ ui-tests/src/test/scala/uk/gov/hmrc/test/ui/cucumber/stepdefs/ApiLoginSteps.scala
@@ -1,7 +1,6 @@
 package uk.gov.hmrc.test.ui.cucumber.stepdefs
 
 import org.apache.pekko.actor.ActorSystem
-import org.apache.pekko.actor.TypedActor.dispatcher
 import org.apache.pekko.stream.Materializer
 import play.api.libs.ws.DefaultBodyWritables.writeableOf_urlEncodedSimpleForm
 import play.api.libs.ws.StandaloneWSResponse
@@ -13,15 +12,14 @@ import scala.concurrent.duration._
 class ApiLoginSteps extends CommonFunctions {
 
   // Shared ActorSystem and Materializer
-  implicit val system: ActorSystem        = ActorSystem("ApiSystem")
+  implicit val system: ActorSystem = ActorSystem("ApiSystem")
   implicit val materializer: Materializer = Materializer(system)
 
-  var responseStatus: Int  = 0
+  var responseStatus: Int = 0
   var responseBody: String = ""
-  var mdtpCookie: String   = ""
 
   Given("""I login to gg sign in page""") { () =>
-    println("Starting login to GG sign-in page")
+    println("This is test")
 
     // Create the WS Client
     val wsClient = StandaloneAhcWSClient()
@@ -47,20 +45,6 @@ class ApiLoginSteps extends CommonFunctions {
         .url(url)
         .addHttpHeaders("Content-Type" -> "application/x-www-form-urlencoded")
         .post(formData)
-        .map { response =>
-          // Validate the response status
-          assert(response.status == 303, s"Unexpected status: ${response.status}")
-
-          // Extract the `mdtp` cookie
-          mdtpCookie = response.header("Set-Cookie")
-            .flatMap("""mdtp=([^;]+)""".r.findFirstMatchIn(_).map(_.group(1)))
-            .getOrElse(throw new RuntimeException("mdtp cookie not found"))
-
-          // Log the extracted cookie
-          println(s"Extracted mdtpCookie: $mdtpCookie")
-
-          response
-        }
 
       // Await the response
       val response: StandaloneWSResponse = Await.result(responseFuture, 10.seconds)
@@ -72,7 +56,6 @@ class ApiLoginSteps extends CommonFunctions {
       // Log response
       println(s"Status: $responseStatus")
       println(s"Body: $responseBody")
-      println(s"mdtpCookie: $mdtpCookie")
 
     } catch {
       case e: Exception =>
