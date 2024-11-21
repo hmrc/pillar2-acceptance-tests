eefc5df6e77d7b2e65b9cd238abba26afb5f18f0
prsathmrc <180431916+prsathmrc@users.noreply.github.com>
2024-11-21 14:05:30 +0000

    ApiRunner and URL changed

--- api-tests/src/test/scala/uk/gov/hmrc/test/ui/cucumber/runners/APIRunner.scala
+++ api-tests/src/test/scala/uk/gov/hmrc/test/ui/cucumber/runners/APIRunner.scala
@@ -24,6 +24,6 @@ import org.junit.runner.RunWith
   features = Array("src/test/resources/features"),
   glue = Array("uk.gov.hmrc.test.ui.cucumber.stepdefs"),
   plugin = Array("pretty", "html:target/cucumber", "json:target/cucumber.json", "junit:target/test-reports/Runner.xml"),
-  tags = "@apiTests and not @ignore"
+  tags = "@tests and not @ignore"
 )
 class APIRunner {}
