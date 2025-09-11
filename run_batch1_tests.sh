562f8b835abac6ee9dc57ef76ea4916b6477ccd3
Prashant Soni <180386844+Prashant-digital@users.noreply.github.com>
2025-09-11 15:05:47 +0100

    PDS-update the sh files for batch jobs (#380)
    Update the 3 batch jobs and run_tests file to run newdashboard tests on
    jenkins

--- run_batch1_tests.sh
+++ run_batch1_tests.sh
@@ -6,4 +6,4 @@ BROWSER="chrome"
 tags="@batch1 and not @ignore"
 echo "*** running on $ENVIRONMENT using $BROWSER for tags '$tags' ***"
 
-sbt clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" -Dcucumber.filter.tags="@batch1 and not @ignore" "testOnly uk.gov.hmrc.test.ui.cucumber.runner.Runner" testReport
+sbt clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" -Dcucumber.filter.tags="@batch1 and not @ignore and not @tests" "testOnly uk.gov.hmrc.test.ui.cucumber.runner.Runner" testReport
--- run_tests.sh
+++ run_tests.sh
@@ -3,4 +3,4 @@
 ENV="local"
 BROWSER="chrome"
 
-sbt clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" -Dcucumber.filter.tags="@newTests" "testOnly uk.gov.hmrc.test.ui.cucumber.runner.Runner" testReport
+sbt clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" -Dcucumber.filter.tags="@newTests and not @ignore and not @tests" "testOnly uk.gov.hmrc.test.ui.cucumber.runner.Runner" testReport
