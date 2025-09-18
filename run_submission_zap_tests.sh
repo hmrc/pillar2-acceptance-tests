a483300a0c8f2f220e8b87b7f4b5a6d5adc3a224
Prashant-digital <180386844+Prashant-digital@users.noreply.github.com>
2025-09-18 15:52:44 +0100

    Removed sh files for p2 tests

--- run_submission_zap_tests.sh
+++
@@ -1,3 +0,0 @@
-echo "Running ZAP tests"
-
-sbt -Dbrowser=remote-chrome -Denvironment=local -Dzap.proxy=true -Dcucumber.filter.tags="@p2zap" clean 'testOnly uk.gov.hmrc.test.ui.cucumber.runner.Runner'
