8b010c655ffaf42da72199817b324d76c15e82af
Prashant Soni <180386844+Prashant-digital@users.noreply.github.com>
2025-11-07 11:13:51 +0000

    PIL-2227 - runner and batch jobs to run new dashboard tests (#396)
    3 batch jobs updated to ignore old dashboard tests, @test tag. 
    tag updated from @tests to @newTests in cucumber runner. 
    
    These two changes to run new dashboard tests in jenkins.

--- run_batch3_tests.sh
+++ run_batch3_tests.sh
@@ -6,4 +6,4 @@ BROWSER="chrome"
 tags="@batch3 and not @ignore"
 echo "*** running on $ENVIRONMENT using $BROWSER for tags '$tags' ***"
 
-sbt clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" -Dcucumber.filter.tags="@batch3 and not @ignore and not @newTests" "testOnly uk.gov.hmrc.test.ui.cucumber.runner.Runner" testReport
+sbt clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" -Dcucumber.filter.tags="@batch3 and not @ignore and not @tests" "testOnly uk.gov.hmrc.test.ui.cucumber.runner.Runner" testReport
