5942f4da46d8d4a0492d4a283aec74ab90953ab0
prsathmrc <180431916+prsathmrc@users.noreply.github.com>
2025-11-12 14:33:04 +0000

    [2570] Changed run_tests file

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-ENV="local"
-BROWSER="chrome"
+#ENV="local"
+#BROWSER="chrome"
+#
+#sbt clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" "testOnly uk.gov.hmrc.test.ui.cucumber.runner.Runner" testReport
 
-sbt clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" "testOnly uk.gov.hmrc.test.ui.cucumber.runner.Runner" testReport
+BROWSER=$1
+ENVIRONMENT=$2
+
+sbt clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" -Dsecurity.assessment=false "testOnly uk.gov.hmrc.test.ui.specs.* -- -n AcceptanceTests" testReport
