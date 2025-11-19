6342a8ce506262a69da53e76de89cfaf5f660dbb
andrew-armstrong80 <35451916+andrew-armstrong80@users.noreply.github.com>
2025-11-19 12:54:11 +0000

    BasePage added Updated RFM pages added

---
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/rfm/RFMContactSecondaryEmailPage.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.test.ui.pages.rfm
+
+import org.openqa.selenium.By
+import uk.gov.hmrc.test.ui.pages.BasePage
+
+object RFMContactSecondaryEmailPage extends BasePage {
+  override val url: String = s"${baseUrl}replace-filing-member/contact-details/second-input-email"
+
+  override val textInputField: By = By.id("emailAddress")
+  private val emailValue: String  = "abc@def.com"
+  private val emailUpdatedValue: String  = "def@abc.com"
+
+  def enterEmail(): Unit = {
+    onPage()
+    sendKeys(textInputField, emailValue)
+    clickByClass(continue)
+  }
+}
---
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/rfm/RFMFinalReviewCYAPage.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.test.ui.pages.rfm
+
+import org.openqa.selenium.By
+import uk.gov.hmrc.test.ui.pages.BasePage
+
+object RFMFinalReviewCYAPage extends BasePage {
+  override val url: String        = s"${baseUrl}replace-filing-member/review-submit/check-answers"
+  override val submitButtonId: By = By.id("send")
+
+  val valueList                     = ".govuk-summary-list__value"
+  val changeCorporatePosition       = "a[href*='/change-corporate-position']"
+  val changeCompany                 = "a[href*='/change-registered-in-uk']"
+  val changeInputName               = "a[href*='/change-input-name']"
+  val changeInputAddress            = "a[href*='/change-input-address']"
+  val changeAddress                 = "[href*='address/change-input']"
+  val changePhoneContact            = "a[href*='/change-phone']"
+  val changeSecondContactPreference = "[href*='change-second-contact']"
+
+}
