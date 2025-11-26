39c0054fab546c94fe82e69f49bffcb4cf747942
andrew-armstrong80 <35451916+andrew-armstrong80@users.noreply.github.com>
2025-11-26 16:10:38 +0000

    BTA Spec and pages refactored and renamed

--- src/test/scala/uk/gov/hmrc/test/ui/pages/auth/AuthLoginPage.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/auth/AuthLoginPage.scala
@@ -23,11 +23,10 @@ import uk.gov.hmrc.test.ui.helper.Input._
 import uk.gov.hmrc.test.ui.helper.{Find, Input, Nav}
 import uk.gov.hmrc.test.ui.pages.BasePage
 import uk.gov.hmrc.test.ui.pages.asa.ASAPillar2InputPage
+import uk.gov.hmrc.test.ui.pages.bta.BTAPillar2IDCheckPage
 import uk.gov.hmrc.test.ui.pages.btn.BtnStartPage
 import uk.gov.hmrc.test.ui.pages.dashboard.DashboardPage
 
-//import uk.gov.hmrc.test.ui.specpages.asa.ASAPillar2InputPage
-//import uk.gov.hmrc.test.ui.specpages.btn.BtnStartPage
 //import uk.gov.hmrc.test.ui.specpages.eligibility.EligibilityUPEPage
 //import uk.gov.hmrc.test.ui.specpages.rfm.{RFMConfirmationPage, RFMStartPage}
 //import uk.gov.hmrc.test.ui.specpages.upe.UPERegisteredInUkPage
@@ -79,7 +78,7 @@ object AuthLoginPage extends BrowserDriver with BasePage {
   def urlMatch(page: String): String = {
     page match {
       case "asa"                 => ASAPillar2InputPage.url
-//      case "bta"                 => BTAPillar2IDCheckPage.url
+      case "bta"                 => BTAPillar2IDCheckPage.url
       case "btn"                 => BtnStartPage.url
       case "dashboard"           => DashboardPage.url
 //      case "eligibility"         => EligibilityUPEPage.url
