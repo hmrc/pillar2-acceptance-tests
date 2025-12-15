8dc41b528d2157088d581994cc1b1aacc04d32a2
andrew-armstrong80 <35451916+andrew-armstrong80@users.noreply.github.com>
2025-12-15 13:34:03 +0000

    Eligibility tests refactored

---
+++ src/test/scala/uk/gov/hmrc/test/ui/specs/EligibilityQuestionsSpec.scala
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.test.ui.specs
+
+import uk.gov.hmrc.test.ui.helper.TestOnlyHelpers
+import uk.gov.hmrc.test.ui.pages.auth.AuthLoginPage.login
+import uk.gov.hmrc.test.ui.pages.eligibility.*
+import uk.gov.hmrc.test.ui.pages.registration.TaskListPage
+import uk.gov.hmrc.test.ui.specs.tags.*
+
+class EligibilityQuestionsSpec extends BaseSpec {
+
+  Feature("Eligibility Questions for Pillar 2") {
+
+    Scenario(
+      "1 - User navigates to confirmation page via the eligibility questions",
+      AcceptanceTests, Wip
+    ) {
+
+      Given("The cache is cleared and an org user signs in to eligibility without enrolment")
+      TestOnlyHelpers.clearSession()
+      login(
+        userType = "Organisation",
+        pageUrl = "eligibility"
+      )
+
+      And("The user answers yes to all eligibility questions")
+      EligibilityUPEPage.selectYes()
+      EligibilityActiveInUkPage.selectYes()
+      EligibilityGlobalGrossRevenueEQPage.selectYes()
+
+      Then("The user is taken to the confirmation page and directed to the register your group page")
+      EligibilityConfirmationPage.onPageSubmitById()
+      TaskListPage.onPage()
+    }
+
+    Scenario(
+      "2 - User navigates to service not suitable as entity not upe or nfm page via the eligibility questions",
+      AcceptanceTests, Wip
+    ) {
+
+      Given("The cache is cleared and an org user signs in to eligibility without enrolment")
+      TestOnlyHelpers.clearSession()
+      login(
+        userType = "Organisation",
+        pageUrl = "eligibility"
+      )
+
+      And("The user answers no to eligibility questions")
+      EligibilityUPEPage.selectNo()
+      EligibilityNFMPage.selectNo()
+
+      Then("The user is taken to the service not suitable as entity not upe or nfm page")
+      EligibilityNotSuitableAsNotUPEOrNFMPage.onPage()
+    }
+
+    Scenario(
+      "3 - User navigates to service not suitable as entity not active in the uk page via the eligibility questions",
+      AcceptanceTests, Wip
+    ) {
+
+      Given("The cache is cleared and an org user signs in to eligibility without enrolment")
+      TestOnlyHelpers.clearSession()
+      login(
+        userType = "Organisation",
+        pageUrl = "eligibility"
+      )
+
+      And("The user answers no to UPE eligibility question")
+      EligibilityUPEPage.selectNo()
+
+      And("The user answers yes to NFM eligibility question")
+      EligibilityNFMPage.selectYes()
+
+      And("The user answers no to active in UK eligibility question")
+      EligibilityActiveInUkPage.selectNo()
+
+      Then("The user is taken to the service not suitable as entity not active in the uk page")
+      EligibilityNotSuitableAsNotActiveInUkPage.onPage()
+
+    }
+
+    Scenario(
+      "4 - User navigates to service not suitable as under 750 million page via the eligibility questions",
+      AcceptanceTests, Wip
+    ) {
+
+      Given("The cache is cleared and an org user signs in to eligibility without enrolment")
+      TestOnlyHelpers.clearSession()
+      login(
+        userType = "Organisation",
+        pageUrl = "eligibility"
+      )
+
+      And("The user answers yes to UPE eligibility question")
+      EligibilityUPEPage.selectYes()
+
+      And("The user answers no to active in UK eligibility question")
+      EligibilityActiveInUkPage.selectYes()
+
+      And("The user answers no to group consolidated annual revenues eligibility question")
+      EligibilityGlobalGrossRevenueEQPage.selectNo()
+
+      Then("The user is taken to the service not suitable as under 750 million page")
+      EligibilityNotSuitableAsUnder750MillionPage.onPage()
+
+    }
+  }
+}
