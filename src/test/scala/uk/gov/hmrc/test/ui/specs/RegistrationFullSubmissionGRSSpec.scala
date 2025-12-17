c0ef2c7dc78afef0bac376fc9efd4771c2df6da4
Andrew Armstrong <35451916+andrew-armstrong80@users.noreply.github.com>
2025-12-17 12:41:46 +0000

    Pil 2580 registration grs (#427)

--- src/test/scala/uk/gov/hmrc/test/ui/specs/RegistrationFullSubmissionGRSSpec.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/specs/RegistrationFullSubmissionGRSSpec.scala
@@ -19,72 +19,20 @@ package uk.gov.hmrc.test.ui.specs
 import uk.gov.hmrc.test.ui.pages.auth.AuthLoginPage.login
 import uk.gov.hmrc.test.ui.pages.contactDetails.*
 import uk.gov.hmrc.test.ui.pages.furtherDetails.*
-import uk.gov.hmrc.test.ui.pages.nfm.NFMRegistrationPage
-import uk.gov.hmrc.test.ui.pages.registration.*
+import uk.gov.hmrc.test.ui.pages.nfm.*
+import uk.gov.hmrc.test.ui.pages.registration.{RegistrationConfirmationPage, ReviewAnswersPage, TaskListPage}
 import uk.gov.hmrc.test.ui.pages.upe.*
 import uk.gov.hmrc.test.ui.specs.tags.*
 
-class RegistrationErrorJourneysSpec extends BaseSpec {
+class RegistrationFullSubmissionGRSSpec extends BaseSpec {
 
-  Feature("Registration Error journeys") {
+  Feature("Full registration using GRS") {
 
-    Scenario(
-      "1 - User redirected to Subscription Failure error page for a duplicate subscription.",
-      AcceptanceTests
+    Scenario("1 - User registers a UK Limited Company UPE successfully",
+      AcceptanceTests, Wip
     ) {
 
-      Given("Organisation User logs in without Pillar2 enrolment")
-      login(
-        userType = "Organisation",
-        page = "pillar2-frontend"
-      )
-
-      And("The user is presented with the task list page")
-      TaskListPage.clickAddUltimateFilingMember()
-
-      And("The user adds a UK Limited Company as an Ultimate Parent Entity")
-      UPEInitialGuidancePage.continueToNextPage()
-      UPERegisteredInUkPage.registeredInUkNo()
-      UPENamePage.enterText(UPENamePage.nameSubscriptionFailureValue)
-      UPEAddressPage.enterAddressNonUK()
-      UPEContactNamePage.enterName()
-      UPEEmailPage.enterEmail()
-      UPEPhoneAsContactPage.selectYes()
-      UPEPhoneInputPage.enterPhoneNumber()
-      UPECheckYourAnswersPage.continueToNextPage()
-
-      And("The user doesn't add a Nominated Filing Member")
-      TaskListPage.clickAddFilingMemberLink()
-      NFMRegistrationPage.nominatedFilingMemberNo()
-
-      And("The user adds further details")
-      TaskListPage.clickAddFurtherGroupDetailsLink()
-      FDGroupStatusPage.selectOnlyUk()
-      FDGroupAccountingPeriodPage.enterDates()
-      FDCheckYourAnswersPage.continueToNextPage()
-
-      And("The user adds primary contact details")
-      TaskListPage.clickAddContactDetailsLink()
-      ContactDetailsGuidancePage.continueToNextPage()
-      ContactDetailsPrimaryContactPage.selectYes()
-      ContactDetailsSecondaryContactAddPage.selectNo()
-      ContactAddressInputPage.enterAddressUK()
-      ContactDetailsCheckAnswersPage.continueToNextPage()
-
-      Then("The user reviews and submits")
-      TaskListPage.clickReviewAndSubmitLink()
-      ReviewAnswersPage.continueToNextPage()
-
-      When("The user is presented with the Subscription Failure error page")
-      RegistrationSubscriptionFailureErrorPage.onPage()
-    }
-
-    Scenario(
-      "2 - User taken to error page for attempting to register an incomplete data submission",
-      AcceptanceTests
-    ) {
-
-      Given("Organisation User logs in without Pillar2 enrolment")
+      Given("Org User logs in without Pillar2 enrolment")
       login(
         userType = "Organisation",
         page = "pillar2-frontend"
@@ -118,30 +66,21 @@ class RegistrationErrorJourneysSpec extends BaseSpec {
       ContactDetailsPrimaryPhoneAsContactPage.selectNo()
       ContactDetailsSecondaryContactAddPage.selectNo()
       ContactAddressInputPage.enterAddressUK()
-
-      And("The user amends the Primary contact as having a phone number via the Check your answers page")
-      ContactDetailsCheckAnswersPage.clickChangeLink(ContactDetailsCheckAnswersPage.changePrimaryContactByPhone)
-      ContactDetailsPrimaryPhoneAsContactPage.selectYes(ContactDetailsPrimaryPhoneAsContactPage.changeUrl)
-
-      And("The user leaves check your answers without adding a phone number")
-      ContactDetailsPrimaryPhonePage.clickOnBackLink(ContactDetailsPrimaryPhonePage.changeUrl)
-      ContactDetailsPrimaryPhoneAsContactPage.clickOnBackLink(ContactDetailsPrimaryPhoneAsContactPage.changeUrl)
       ContactDetailsCheckAnswersPage.continueToNextPage()
 
       Then("The user reviews and submits")
       TaskListPage.clickReviewAndSubmitLink()
       ReviewAnswersPage.continueToNextPage()
 
-      When("The user is presented with the Incomplete Data Submission error page")
-      RegistrationIncompleteDataErrorPage.onPage()
+      When("The user is presented with the Registration Complete page")
+      RegistrationConfirmationPage.onPage(timeoutSeconds = 10)
     }
 
-    Scenario(
-      "3 - User redirected to the API error page for an invalid request during registration.",
-      AcceptanceTests
+    Scenario("2 - User registers a UK Limited Company as ",
+      AcceptanceTests, Wip
     ) {
 
-      Given("Organisation User logs in without Pillar2 enrolment")
+      Given("Org User logs in without Pillar2 enrolment")
       login(
         userType = "Organisation",
         page = "pillar2-frontend"
@@ -150,10 +89,10 @@ class RegistrationErrorJourneysSpec extends BaseSpec {
       And("The user is presented with the task list page")
       TaskListPage.clickAddUltimateFilingMember()
 
-      And("The user adds a UK Limited Company as an Ultimate Parent Entity")
+      And("The user manually adds an Ultimate Parent Entity with a non-UK Address")
       UPEInitialGuidancePage.continueToNextPage()
       UPERegisteredInUkPage.registeredInUkNo()
-      UPENamePage.enterText(UPENamePage.nameAPIErrorValue)
+      UPENamePage.enterName()
       UPEAddressPage.enterAddressNonUK()
       UPEContactNamePage.enterName()
       UPEEmailPage.enterEmail()
@@ -161,14 +100,19 @@ class RegistrationErrorJourneysSpec extends BaseSpec {
       UPEPhoneInputPage.enterPhoneNumber()
       UPECheckYourAnswersPage.continueToNextPage()
 
-      And("The user doesn't add a Nominated Filing Member")
+      And("The user adds a UK Limited Company as a Nominated Filing Member")
       TaskListPage.clickAddFilingMemberLink()
-      NFMRegistrationPage.nominatedFilingMemberNo()
+      NFMRegistrationPage.nominatedFilingMemberYes()
+      NFMRegisteredInUkPage.registeredInUkYes()
+      NFMEntityTypePage.enterEntityTypeUkLimitedCompany()
+      NFMUKCompanyGRSPage.clickRegistrationSuccessBvEnabled()
 
       And("The user adds further details")
       TaskListPage.clickAddFurtherGroupDetailsLink()
       FDGroupStatusPage.selectOnlyUk()
       FDGroupAccountingPeriodPage.enterDates()
+
+      Then("The user continues from further details to the task list")
       FDCheckYourAnswersPage.continueToNextPage()
 
       And("The user adds primary contact details")
@@ -183,8 +127,8 @@ class RegistrationErrorJourneysSpec extends BaseSpec {
       TaskListPage.clickReviewAndSubmitLink()
       ReviewAnswersPage.continueToNextPage()
 
-      When("The user is presented with the API error page")
-      RegistrationAPIErrorPage.onPage()
+      When("The user is presented with the Registration Complete page")
+      RegistrationConfirmationPage.onPage(timeoutSeconds = 10)
     }
   }
 }
