e7fbd0635d9141932b3888f2d64d3da7781c60f8
andrew-armstrong80 <35451916+andrew-armstrong80@users.noreply.github.com>
2025-12-17 12:02:29 +0000

    Remaining BTN journeys refactored

--- src/test/scala/uk/gov/hmrc/test/ui/specs/BTNSubmissionSpec.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/specs/BTNSubmissionSpec.scala
@@ -19,14 +19,17 @@ import uk.gov.hmrc.test.ui.pages.asa.*
 import uk.gov.hmrc.test.ui.pages.auth.AuthLoginPage.{DelegatedEnrolment, Enrolment, login}
 import uk.gov.hmrc.test.ui.pages.btn.*
 import uk.gov.hmrc.test.ui.pages.dashboard.DashboardPage
-import uk.gov.hmrc.test.ui.specs.tags.AcceptanceTests
+import uk.gov.hmrc.test.ui.specs.tags.*
 
 class BTNSubmissionSpec extends BaseSpec {
 
   Feature("Below threshold notification successful submission user journeys") {
 
-    Scenario("1 - Org User makes successful below threshold notification submission", AcceptanceTests) {
-      Given("Organisation User logs in to Pillar2 service")
+    Scenario("1 - Org User makes successful below threshold notification submission",
+      AcceptanceTests
+    ) {
+
+      Given("Org User logs in to Pillar2 service")
       login(
         userType = "Organisation",
         page = "dashboard",
@@ -54,7 +57,10 @@ class BTNSubmissionSpec extends BaseSpec {
       BtnCannotReturnPage.onPage()
     }
 
-    Scenario("2 - Agent User makes successful below threshold notification submission", AcceptanceTests) {
+    Scenario("2 - Agent User makes successful below threshold notification submission",
+      AcceptanceTests
+    ) {
+
       Given("Agent User logs in to Pillar2 service with delegated enrollment")
       login(
         userType = "Agent",
@@ -95,51 +101,11 @@ class BTNSubmissionSpec extends BaseSpec {
       BtnCannotReturnPage.onPage()
     }
 
-    Scenario("3 - Org user navigates to Under Enquiry Page when BTN submission is attempted with enquiry flag true", AcceptanceTests) {
+    Scenario(
+      "3 - Agent User makes successful BTN submission for current accounting period of multiple accounting periods",
+      AcceptanceTests
+    ) {
 
-      Given("Organisation User logs in to Pillar2 service")
-      login(
-        userType = "Organisation",
-        page = "dashboard",
-        enrolment = Some(
-          Enrolment(
-            "HMRC-PILLAR2-ORG",
-            "PLRID",
-            "XEPLR9999999995"
-          )
-        )
-      )
-
-      Then("The user starts a below threshold notification")
-      DashboardPage.clickSubmitBTNLink()
-      BtnStartPage.continueToNextPage()
-
-      And("The user selects the current accounting period")
-      BtnMultipleAccountingPage.selectCurrentAccountingPeriod()
-
-      And("The user returns to the accounting period and selects previous accounting period and reaches BTN under enquiry page")
-      BtnAccountingPage.clickOnBackLink()
-      BtnMultipleAccountingPage.selectPreviousAccountingPeriod()
-
-      And("The user returns to the multiple accounting period and select PreviousAccountingPeriodUKTRSubmitted")
-      BtnUnderEnquiryPage.clickBackButton()
-      BtnMultipleAccountingPage.selectPreviousAccountingPeriodUKTRSubmitted()
-
-      Then("The user reaches the BTN under enquiry page and continues BTN submission")
-      BtnUnderEnquiryPage.continueToNextPage()
-      BtnAccountingPage.continueToNextPage()
-      BtnDomesticOrMnePage.entityInAndOutUkYes()
-      BtnCyaSubmitPage.onPageSubmitById()
-
-      And("The user is on the Below-Threshold Notification successful confirmation page")
-      BtnConfirmationPage.onPage(timeoutSeconds = 10)
-
-      When("The user returns to the dashboard")
-      BtnConfirmationPage.clickReturnToHomepageLink()
-      DashboardPage.onPage()
-    }
-
-    Scenario("4 - Agent user navigates to Under Enquiry Page when BTN submission is attempted with enquiry flag true", AcceptanceTests) {
       Given("Agent User logs in with existing entity group")
       login(
         userType = "Agent",
@@ -155,14 +121,14 @@ class BTNSubmissionSpec extends BaseSpec {
           DelegatedEnrolment(
             key = "HMRC-PILLAR2-ORG",
             name = "PLRID",
-            value = "XEPLR9999999995",
+            value = "XEPLR9999999994",
             authRule = "pillar2-auth"
           )
         )
       )
 
       Then("The agent confirms client PLRId")
-      ASAPillar2InputPage.enterPLR2Id("XEPLR9999999995")
+      ASAPillar2InputPage.enterPLR2Id("XEPLR9999999994")
       ASAConfirmationPage.continueToNextPage()
 
       Then("The agent starts a below threshold notification")
@@ -171,27 +137,56 @@ class BTNSubmissionSpec extends BaseSpec {
 
       And("The agent selects the current accounting period")
       BtnMultipleAccountingPage.selectCurrentAccountingPeriod()
-
-      And("The agent returns to the multiple accounting period and selects previous accounting period and reached BtnUnderEnquiryPage")
-      BtnAccountingPage.clickOnBackLink()
-      BtnMultipleAccountingPage.selectPreviousAccountingPeriod()
-
-      And("The agent returns to the multiple accounting period and select PreviousAccountingPeriodUKTRSubmitted")
-      BtnUnderEnquiryPage.clickBackButton()
-      BtnMultipleAccountingPage.selectPreviousAccountingPeriodUKTRSubmitted()
-
-      And("The agent continue from BtnUnderEnquiryPage ")
-      BtnUnderEnquiryPage.continueToNextPage()
       BtnAccountingPage.continueToNextPage()
       BtnDomesticOrMnePage.entityInAndOutUkYes()
       BtnCyaSubmitPage.onPageSubmitById()
 
-      And("The agent is on the Below-Threshold Notification successful confirmation page")
+      When("The user is on the Below-Threshold Notification successful confirmation page")
       BtnConfirmationPage.onPage(timeoutSeconds = 10)
+    }
 
-      When("The agent returns to the dashboard")
-      BtnConfirmationPage.clickReturnToHomepageLink()
-      DashboardPage.onPage()
+    Scenario(
+      "4 - Agent User makes successful BTN submission for previous accounting period of multiple accounting periods",
+      AcceptanceTests
+    ) {
+
+      Given("Agent User logs in with existing entity group")
+      login(
+        userType = "Agent",
+        page = "asa",
+        enrolment = Some(
+          Enrolment(
+            key = "HMRC-AS-AGENT",
+            name = "AgentReference",
+            value = "1234"
+          )
+        ),
+        delegatedEnrolments = Seq(
+          DelegatedEnrolment(
+            key = "HMRC-PILLAR2-ORG",
+            name = "PLRID",
+            value = "XEPLR9999999994",
+            authRule = "pillar2-auth"
+          )
+        )
+      )
+
+      Then("The agent confirms client PLRId")
+      ASAPillar2InputPage.enterPLR2Id("XEPLR9999999994")
+      ASAConfirmationPage.continueToNextPage()
+
+      Then("The agent starts a below threshold notification")
+      DashboardPage.clickSubmitBTNLink()
+      BtnStartPage.continueToNextPage()
+
+      And("The agent selects the previous accounting period")
+      BtnMultipleAccountingPage.selectPreviousAccountingPeriod()
+      BtnAccountingPage.continueToNextPage()
+      BtnDomesticOrMnePage.entityInAndOutUkYes()
+      BtnCyaSubmitPage.onPageSubmitById()
+
+      When("The user is on the Below-Threshold Notification successful confirmation page")
+      BtnConfirmationPage.onPage(timeoutSeconds = 10)
     }
   }
 }
